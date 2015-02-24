00b2984fd53ca3ef99dc327aaed587c33dce2351
AlexSYanai <yanai.alex@gmail.com>
2015-02-23 23:42:01 -0500

    completes challenge 31

--- challenge_31.rb
+++ challenge_31.rb
@@ -1,6 +1,7 @@
-coins = [1,2,5,10,20,50,100,200]
-coin_counter = [0,0,0,0,0,0,0,0]
-target = 200
-
-
-
+ def count_coins(target)
+   coins = [1,2,5,10,20,50,100,200]
+   ways = ["1" + "0"*200].map{ |n| n.split("").map { |n| n.to_i } }.flatten
+   coins.each { |coin| (coin..target).each { |num|  ways[num] += ways[num-coin] } }
+   ways[target]
+ end
+p count_coins(200)
