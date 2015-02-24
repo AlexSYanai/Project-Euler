9d80330f7b26f31fdee9faf889434f157acbe425
AlexSYanai <yanai.alex@gmail.com>
2015-02-24 08:45:18 -0500

    makes minor change to search range in 32

--- challenge_32.rb
+++ challenge_32.rb
@@ -9,7 +9,7 @@ class Pandigital
   def find_products
     (2..100).each do |x|
       start = (x<9) ? 1234 : 123
-      (start..(10000/x)).each do |y|
+      (start..(10000/x-2)).each do |y|
         current_product = x*y
         converted = current_product.to_s + x.to_s + y.to_s
         product_array << current_product unless product_array.include?(current_product) if is_pandigital?(converted)
