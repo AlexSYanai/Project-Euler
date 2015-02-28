70f5c20e71f33b2fe95df255c3ee9350fb923e38
AlexSYanai <yanai.alex@gmail.com>
2015-02-28 18:57:10 -0500

    adds challenge 38 solution and challenge 39

--- challenge_38.rb
+++ challenge_38.rb
@@ -1,5 +1,5 @@
 class PandigitalMultiples
-  attr_accessor :product_array,:final_sum,:all_pandigitals
+  attr_accessor :all_pandigitals
   attr_reader   :number_strings
   def initialize
     @number_strings  = %w(1 2 3 4 5 6 7 8 9)
@@ -10,15 +10,16 @@ class PandigitalMultiples
     (9000..9999).each { |n| find_products(n) }
   end
 
-  def find_products(num)
-    prime1 = num.to_s
-    counter = 2
+  def max_pandigital
+    all_pandigitals.max.to_i
+  end
 
-    while counter < 4
-      idk = num*counter
-      prime1 += idk.to_s
-      all_pandigitals << prime1 if is_pandigital?(prime1)
-      counter += 1
+  private
+  def find_products(num)
+    prime = num.to_s
+    (2..4).each do |n|
+      prime += (num*n).to_s
+      all_pandigitals << prime if is_pandigital?(prime)
     end
   end
 
@@ -29,4 +30,4 @@ end
 
 euler = PandigitalMultiples.new
 euler.check_numbers
-p euler.all_pandigitals
+p euler.max_pandigital
