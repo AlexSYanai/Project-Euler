d0663b55f91407428659e0916ed14332636a8c9e
AlexSYanai <yanai.alex@gmail.com>
2015-03-06 03:34:22 -0500

    fixes method name error and slightly modifies 67

--- challenge_67.rb
+++ challenge_67.rb
@@ -1,30 +1,28 @@
 class TrianglePath # Essentially Solution to #13 w/slight changes
   attr_reader   :filename
-  attr_accessor :triangle_array
+  attr_accessor :tri_nums
 
   def initialize(filename)
-    @filename       = filename
-    @triangle_array = []
+    @filename = filename
+    @tri_nums = []
   end
 
   def parse_file
     File.open(filename,'r').each_line do |line|
-      triangle_array << line.chomp.split(" ").map { |number| number.to_i }
+      tri_nums << line.chomp.split(" ").map { |number| number.to_i }
     end
   end
 
   def find_path
-    i = (triangle_array.length - 1) #Start from the bottom and find the best last row pairs for each last-1th row element
-    while i > 0
-      for j in (0..(i-1))           #Iterate through each row and compare numbers in a pairwise fashion
-        triangle_array[i-1][j] += [triangle_array[i][j],triangle_array[i][j+1]].sort.pop #Add highest to 1st row element
+    (tri_nums.length - 1).downto(0) do |i|  # Start from the bottom and find the best last row pairs for each last-1th row element
+      (0..(i-1)).each do |j|                # Iterate through each row and compare numbers in a pairwise fashion
+        tri_nums[i][j] > tri_nums[i][j+1] ? tri_nums[i-1][j] += tri_nums[i][j] : tri_nums[i-1][j] += tri_nums[i][j+1] # Add highest to 1st row element
       end
-      i -= 1
     end
-    puts triangle_array.first.first
+    puts tri_nums.first.first
   end
 end
 
 yodle_triangle = TrianglePath.new('challenge_67_triangle.txt')
-yodle_triangle.file_parser
+yodle_triangle.parse_file
 yodle_triangle.find_path
