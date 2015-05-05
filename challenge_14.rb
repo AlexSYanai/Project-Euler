e1722d271eb3e5b50b5da6b3f0d161ad7f1db7b4
AlexSYanai <yanai.alex@gmail.com>
2015-05-05 10:58:56 -0400

    updates 14

--- challenge_14.rb
+++ challenge_14.rb
@@ -1,32 +1,28 @@
 class Collatz 	#Brute force - don't know if it's optimized but it works pretty quickly.
-	attr_accessor :longest_sequence, :number_array, :longest_path_number
+	attr_accessor :longest_sequence,:longest_path_number
+	attr_reader   :number_array
 	def initialize
-		@number_array = (1..1000000).select{|n| n.odd?}
+		@number_array = (1..1000000).select { |n| n.odd? }
 		@longest_sequence = 0
 		@longest_path_number = 0
 	end
 
-	def find
-		for n in number_array
+	def find_longest_sequence
+		number_array.each do |n|
 			holder = n
-			counter = 0
+			i = 0
 			until holder <= 1
-				if holder.odd?
-					holder = (holder*3) + 1
-				else
-					holder /= 2
-				end
-				 counter += 1
+				holder.odd? ? holder = ((holder*3) + 1) : (holder /= 2)
+			 	i += 1
 			end
-			if counter > longest_sequence
+			if i > longest_sequence
 				@longest_path_number = n
-				@longest_sequence = counter
+				@longest_sequence = i
 			end
 		end
 	end
 end
 
 euler = Collatz.new
-euler.find
-p euler.longest_sequence
+euler.find_longest_sequence
 p euler.longest_path_number
