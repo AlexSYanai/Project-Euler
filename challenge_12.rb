28d3b5f7b5524bcb1bdfaf36964b495afeb30326
AlexSYanai <yanai.alex@gmail.com>
2015-05-05 10:31:06 -0400

    optimizes challenge 12

--- challenge_12.rb
+++ challenge_12.rb
@@ -1,40 +1,65 @@
-def num_divisors(n)
-  count = 0
-  divisors = 1
-  p = 3
-
-  if n % 2 == 0
-    n = n/2
-    while n % 2 == 0
-      count += 1
-      n /= 2
-    end
-    divisors *= (count + 1)
+require 'benchmark'
+class TriangleNumbers
+  attr_accessor :prime_array,:triangle_num,:divs,:num_divs,:current_prime
+  attr_reader   :target_divs
+  def initialize(target_divs)
+    @target_divs   = target_divs
+    @prime_array   = []
+    @divs          = []
+    @triangle_num  = 1
+    @num_divs      = 0
+    @current_prime = 3
   end
 
-  until n == 1
+  def find_triangle_numbers
+    while num_divs <= target_divs
+      @triangle_num = (current_prime*(current_prime+1)/2)
+      test_triangle(triangle_num)
+      @num_divs = divs.map { |i| i += 1 }.inject(:*)
+      @current_prime += 1
+    end
+  end
+
+  def test_triangle(tri_num)
     count = 0
-    while n % p == 0
-      count += 1
-      n /= p
+    factors = []
+    @divs = []
+    i = 0
+    while tri_num > 1
+      if (tri_num % @prime_array[i]) == 0
+        factors << @prime_array[i] unless factors.include?(@prime_array[i])
+        count += 1
+        tri_num /= @prime_array[i]
+        divs << count if tri_num == 1
+      else
+        count > 0 ? divs << count : nil
+        count = 0
+        i += 1
+      end
     end
-    divisors *= (count + 1)
-    p += 2
   end
-  divisors
-end
 
-def find_triangular_index
-  n = 1
-  lower_number = num_divisors(n)
-  higher_number = num_divisors(n+1)
-
-  while lower_number * higher_number < 500
-    n += 1
-    lower_number = higher_number
-    higher_number = num_divisors(n+1)
+  def sieve_of_eratosthenes
+    (0..(target_divs*25)).each { |i| prime_array[i] = i+2 }
+    index = 0
+    while prime_array[index]**2 <= prime_array.last
+      @prime_array = prime_array.select { |x| x == prime_array[index] || x % prime_array[index] != 0 }
+      index += 1
+    end
   end
-  n
 end
+# Benchmark.bm do |a|
+#   a.report {
+    euler = TriangleNumbers.new(500)
+    euler.sieve_of_eratosthenes
+    euler.find_triangle_numbers
+    p euler.triangle_num
+#   }
+# end
 
-p (find_triangular_index * (find_triangular_index + 1)) / 2
+# Old
+# user     system      total        real
+# 1.420000   0.000000   1.420000 (  1.421089)
+# New
+# user     system      total        real
+# 0.370000   0.000000   0.370000 (  0.362893)
