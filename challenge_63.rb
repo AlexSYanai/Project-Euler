6dde98cd1ab26454bdc8133019efa6103474bd20
AlexSYanai <yanai.alex@gmail.com>
2015-06-02 02:19:47 -0400

    adds challenge 63

---
+++ challenge_63.rb
@@ -0,0 +1,26 @@
+# Easier to solve with pen and paper
+class Integer
+	def same_digit_length?(x)     # Tests digit count against the exponent 
+		(self**x).to_s.length == x
+	end
+end
+
+exp = 1
+num = 1
+count = 0
+
+while 9.same_digit_length?(exp)   # Upper bound is 9 => 10**x will always
+	unless num == 9 								# have a product 1 digit greater in length
+		(num..9).each do |n|
+			if n.same_digit_length?(exp)
+				count += 1
+			else
+				num = n if n > num 				# Once a product digit length is less than
+			end 												# the exponent it never becomes equal to it
+		end 													# Can reset the lower bound if n > num
+	else
+		count += 1
+	end
+	exp += 1
+end
+p count
