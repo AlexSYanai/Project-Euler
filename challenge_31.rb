1a855890f5131af5bfde8d0ae9d7a2e00d36db4d
AlexSYanai <yanai.alex@gmail.com>
2015-06-16 08:29:05 -0400

    adds challenge 76

--- challenge_31.rb
+++ challenge_31.rb
@@ -1,6 +1,6 @@
  def count_coins(target)
    coins = [1,2,5,10,20,50,100,200]
-   ways = ["1" + "0"*200].map { |n| n.split("").map(&:to_i) }.flatten
+   ways = Array.new(target) { 0 }.unshift(1)
    ways = coins.map { |coin| (coin..target).map { |num|  ways[num] += ways[num-coin] } }.last.last
  end
 p count_coins(200)
