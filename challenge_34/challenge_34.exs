0657d588003976f56cb733651ca5ac98c7f15df5
AlexSYanai <yanai.alex@gmail.com>
2015-08-21 00:53:49 -0400

    challenge 34 elixir

---
+++ challenge_34/challenge_34.exs
@@ -0,0 +1,28 @@
+defmodule Factorial do 
+	def find(count,facts) when count > 9, do: [Enum.reverse(facts),facts]
+	def find(count,[last|facts]) when count <= 9 do
+		find(count+1,[count*last] ++ [last] ++ facts)
+	end
+end
+
+defmodule DigitFactorials do
+	def find(count,_,lim,sum) when count > lim do
+		sum
+	end
+
+	def find(count,facts,lim,sum) when count <= lim do
+		if digit_factorial?(count,facts), do: sum = sum + count
+		find(count+1,facts,lim,sum)
+	end
+
+	def digit_factorial?(num,facts) do
+		num == Integer.to_string(num)
+			|> String.split("",trim: true)
+			|> Enum.map(fn n -> Enum.at(facts,String.to_integer(n)) end)
+			|> Enum.reduce(fn a,b -> a + b end)
+	end
+end
+
+[facts,[lim|_]] = Factorial.find(1,[1])
+IO.inspect DigitFactorials.find(3,facts,lim,0)
+
