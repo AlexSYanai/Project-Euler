6705a4a9e32a9bde820fc8d9a7614848f4536f0b
AlexSYanai <yanai.alex@gmail.com>
2015-08-23 23:07:27 -0400

    challenge 32 in elixir

---
+++ challenge_32/challenge_32.exs
@@ -0,0 +1,32 @@
+defmodule Pandigital do
+	def find(num,prods) when num > 100  do 
+		prods
+			|> Enum.uniq()
+			|> Enum.reduce(0,fn a,b -> a + b end)
+	end
+
+	def find(num,prods) when num <= 100 do 
+		if num < 9, do: start = 1234, else: start = 123
+
+		find(num+1,products(num,prods,start,div(10000,(num-2))))
+	end
+
+	def products(_,prods,start,lim) 	when start > lim do
+		prods
+	end
+
+	def products(num,prods,start,lim) when start <= lim do
+		current = num*start
+		convert = Integer.to_string(current) <> Integer.to_string(num) <> Integer.to_string(start)
+		if pandigital?(convert), do: prods = [current] ++ prods
+		products(num,prods,start+1,lim)
+	end
+
+	def pandigital?(n) do
+		~w(1 2 3 4 5 6 7 8 9) == n 
+			|> String.split("", trim: true) 
+			|> Enum.sort()
+	end 
+end
+
+IO.inspect Pandigital.find(3,[])
