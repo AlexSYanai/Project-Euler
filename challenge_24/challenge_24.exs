d567d41a3abfd7a0db5cbfaf014525c8090b5eab
AlexSYanai <yanai.alex@gmail.com>
2015-08-25 01:16:51 -0400

    added challenge 31

--- challenge_24/challenge_24.exs
+++ challenge_24/challenge_24.exs
@@ -1,4 +1,4 @@
-defmodule RC do
+defmodule Permutation do
 	def find(list) do
 		a = permute(list)
 		Enum.at(a,999999)
@@ -13,4 +13,4 @@ defmodule RC do
   end
 end
  
-IO.inspect RC.find([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+IO.inspect Permutation.find([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
