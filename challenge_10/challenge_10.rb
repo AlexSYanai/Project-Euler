c24ac01587e39a8878bc262e054dfd2bca497e7e
AlexSYanai <yanai.alex@gmail.com>
2015-09-11 01:14:36 -0400

    updated ruby challenge 10

--- challenge_10/challenge_10.rb
+++ challenge_10/challenge_10.rb
@@ -1,28 +1,55 @@
-class PrimeCache
-	attr_accessor :finish
-	attr_reader   :primes
-	def initialize(finish)
-		@finish = finish
-		@primes = [2,3]
+class SieveofAtkin
+  attr_accessor :primes, :sieve
+  attr_reader   :limit,  :finish
+  def initialize(limit)
+    @limit  = limit
+    @finish = Math.sqrt(limit).round
+    @primes = [2,3]
+    @sieve  = Array.new(limit+1) { false }
+  end
+
+  def use_sieve
+    1.upto(finish+1) do |i|
+      1.upto(finish+1) do |j|
+        first_60_check( 4*i**2 + j**2)
+        second_60_check(3*i**2 + j**2)
+        third_60_check((3*i**2 - j**2)) if i > j
+      end
+    end
+    check_sieve
+    check_primes
 	end
 
-	def get_primes_upto
-		return print_prime_sums if primes.last == finish 
-		primes.last > finish ? @primes.select(:<) : (@primes.last..finish).step(2).map { |x| @primes << x if is_prime?(x) }
-	end
+  def print_prime_sums
+    p primes.inject(:+)
+  end
+  
+  private
+  def first_60_check(n)
+    @sieve[n] = !sieve[n] if n <= limit && (n % 12 == 1 || n % 12 == 5)
+  end
 
-	def is_prime?(num)
-		primes.each do |prime|
-			return false if num % prime == 0
-			return true  if prime > Math.sqrt(num).round
-		end
-	end
+  def second_60_check(n)
+    @sieve[n] = !sieve[n] if n <= limit &&  n % 12 == 7
+  end
 
-	def print_prime_sums
-		primes.inject(:+)
-	end
+  def third_60_check(n)
+    @sieve[n] = !sieve[n] if n <= limit &&  n % 12 == 11
+  end
+
+  def check_sieve
+    5.upto(finish) { |n| set_primes(n) if sieve[n] }
+  end
+
+  def check_primes
+    5.upto(limit)  { |n| @primes << n  if sieve[n] }
+  end
+
+  def set_primes(x)
+    ((x**2)..(limit+1)).step(x**2).each { |n| @sieve[n] = false }
+  end
 end
 
-idk = PrimeCache.new(2000000)
-idk.get_primes_upto
-p idk.print_prime_sums
+euler = SieveofAtkin.new(2000000)
+euler.use_sieve
+euler.print_prime_sums
