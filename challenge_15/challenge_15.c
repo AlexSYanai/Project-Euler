151edaa949f43cdc0acce2dfa88eefae324cefd8
AlexSYanai <yanai.alex@gmail.com>
2015-10-07 13:00:33 -0400

    adding 3 more problems in C

---
+++ challenge_15/challenge_15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+double factorial(int n) 
+{ 
+	if (n <= 1) {
+		return 1;
+	}
+
+	return n * factorial(n-1); 
+} 
+
+int main() 
+{ 
+	double a,b;
+
+	a = factorial(40); 
+	b = factorial(20) * factorial(20);
+
+	printf("%F\n", a/b); 
+	return 0; 
+} 
