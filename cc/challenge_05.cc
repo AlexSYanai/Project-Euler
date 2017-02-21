d3302e2ff6ce2eb198029f0e41df7f3f0a63ef45
Alex Yanai <yanai.alex@gmail.com>
2017-02-21 07:46:50 -0500

    removed binaries for c++

--- cc/challenge_05.cc
+++ cc/challenge_05.cc
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <functional>
+#include <numeric>
+#include <vector>
 
-bool IsNotMultiple(const int n) {
-  return n %  2 != 0 || n %  3 != 0 || n %  4 != 0 ||
-         n %  5 != 0 || n %  6 != 0 || n %  7 != 0 ||
-         n %  8 != 0 || n %  9 != 0 || n % 10 != 0 ||
-         n % 11 != 0 || n % 12 != 0 || n % 13 != 0 ||
-         n % 14 != 0 || n % 15 != 0 || n % 16 != 0 ||
-         n % 17 != 0 || n % 18 != 0 || n % 19 != 0 ||
-         n % 20 != 0;
+#include "sieve.h"
+
+bool IsMultiple(const int n) {
+  return n %  2 == 0 && n %  3 == 0 && n %  4 == 0 &&
+         n %  5 == 0 && n %  6 == 0 && n %  7 == 0 &&
+         n %  8 == 0 && n %  9 == 0 && n % 10 == 0 &&
+         n % 11 == 0 && n % 12 == 0 && n % 13 == 0 &&
+         n % 14 == 0 && n % 15 == 0 && n % 16 == 0 &&
+         n % 17 == 0 && n % 18 == 0 && n % 19 == 0 &&
+         n % 20 == 0;
 }
 
 int SmallestMultiple(const int lim) {
-  int multi = lim;
+  std::vector<int> primes = Sieve(lim);
+  int multi = std::accumulate(primes.begin(), primes.end(), 1, std::multiplies<>());
 
-  while (IsNotMultiple(multi)) {
-    multi += 20;
+  for (int i = 4; i < lim + 1; i += 2) {
+    multi *= i;
+
+    if (IsMultiple(multi)) {
+      break;
+    }
   }
 
   return multi;
